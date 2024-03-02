ab1676c06b390f97c1e3962f56b79e07878fab15
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-02 11:03:59 +0400

    Create chap 1 ex 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/chap 1 ex 3.py
@@ -0,0 +1,5 @@
+# importing datetime module and printing current datetime
+import datetime
+now = datetime.datetime.now()
+print("current date and time is:")
+print (now.strftime("%d-%m-%Y %H:%M:%S"))
