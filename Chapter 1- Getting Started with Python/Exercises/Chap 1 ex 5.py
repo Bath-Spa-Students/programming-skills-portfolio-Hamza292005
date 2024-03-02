e9265e0e4541bb786db07ee06b32e013d5e9959a
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-02 11:04:10 +0400

    Create Chap 1 ex 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Chap 1 ex 5.py
@@ -0,0 +1,5 @@
+# taking radii and printing area of circle
+import math
+radii = float(input("Enter the radius of circle: "))
+area = math.pi * radii * radii
+print("Area of circle is : {0}".format(area))
