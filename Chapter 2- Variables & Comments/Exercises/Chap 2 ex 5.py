4ee7e6d80f615562b60c71433957f9261138745d
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-08 22:37:10 +0400

    Create Chap 2 ex 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chap 2 ex 5.py
@@ -0,0 +1,19 @@
+# Assigning cost of USB stick and total budget
+
+cost=6
+
+budget=50
+
+# Calculating the max amount of usb sticks she can buy
+
+num_USB= budget//cost
+
+#calculate the amount left
+
+amount_left = budget%cost
+
+#printing results
+
+print("Number of USB sticks dhe can buy:",num_USB)
+
+print("Pounds left after buying USB sticks:",amount_left)
