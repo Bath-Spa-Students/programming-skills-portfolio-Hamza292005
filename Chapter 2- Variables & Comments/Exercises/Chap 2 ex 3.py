69eb1f534670f5211afd18927780e330000615d5
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-08 22:37:01 +0400

    Create Chap 2 ex 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chap 2 ex 3.py
@@ -0,0 +1,19 @@
+# defining a variable with whitespace
+
+name = "\t \n Hamza ahmed \t \n"
+
+# printing the name with whitespace
+ 
+print ("name with whitespace:", name) 
+
+#using lstrip()
+
+print(name.lstrip())
+
+#using rstrip()
+
+print(name.rstrip())
+
+#using strip()
+
+print(name.strip())
