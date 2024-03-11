4f529511488b075ab8bd67afaa1975e30e4ced47
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-11 17:02:57 +0400

    Create Chap 3 ex 1.py

---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex 1.py
@@ -0,0 +1,13 @@
+# storing my friend's name in a list
+
+names = ["Abdur rahman","Abdullah","Waleed","Fahad"]
+
+#printing the names
+
+print(names[0])
+
+print(names[1])
+
+print(names[2])
+
+print(names[3])
