33afca4be60bece5b2d5d833c6cc4b405588f22e
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-11 17:03:29 +0400

    Create Chap 3 ex5.py

---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex5.py
@@ -0,0 +1,31 @@
+# making a guest list
+
+Guest_list = ["Abdur rahman","Abdullah","Waleed","Fahad"]
+
+#Inviting each of them to dinner
+
+print("dear " + Guest_list[0] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[1] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[2] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[3] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+#Printing the name of guest who isnt coming
+
+print("It seems " + Guest_list[0] + " will not make it to dinner")
+
+#Updating the list
+
+Guest_list = ["Faiz","Abdullah","Waleed","Fahad"]
+
+#Printing invitations again
+
+print("dear " + Guest_list[0] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[1] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[2] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[3] + ", I would like to inform you that you're invited to my dinner tommorow")
