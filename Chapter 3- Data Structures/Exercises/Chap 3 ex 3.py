6179609582a41d1f15777e32354d884e31616b40
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-11 17:03:14 +0400

    Create Chap 3 ex 3.py

---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex 3.py
@@ -0,0 +1,11 @@
+# making a list of motorcycles
+
+Motorcycles = ["Kawasaki ninja","Honda","Suzuki"]
+
+#Printing a statement about each one
+
+print(Motorcycles[0] + "'s engine is really powerful") 
+
+print("I would like to ride a " + Motorcycles[1] + " Bike one day")
+
+print(Motorcycles[2] + " bikes are really common in Pakistan")
