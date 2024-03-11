42533c4251d1e67ffd7472cad46bec9766307e83
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-11 17:03:21 +0400

    Create Chap 3 ex 4.py

---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex 4.py
@@ -0,0 +1,13 @@
+# making a guest list
+
+Guest_list = ["Abdur rahman","Abdullah","Waleed","Fahad"]
+
+#Inviting each of them to dinner
+
+print("dear " + Guest_list[0] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[1] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[2] + ", I would like to inform you that you're invited to my dinner tommorow")
+
+print("dear " + Guest_list[3] + ", I would like to inform you that you're invited to my dinner tommorow")
