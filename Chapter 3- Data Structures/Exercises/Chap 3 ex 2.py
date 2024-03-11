992f3be944f045a0f87f43a69117f67fba06b46b
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-11 17:03:08 +0400

    Create Chap 3 ex 2.py

---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex 2.py
@@ -0,0 +1,13 @@
+# using the same list 
+
+names = ["Abdur rahman","Abdullah","Waleed","Fahad"]
+
+# printing persnolized message to each friend
+
+print("hey! " + names[0] + ",how are you?")
+
+print("hey! " + names[1] + ",how are you?")
+
+print("hey! " + names[2] + ",how are you?")
+
+print("hey! " + names[3] + ",how are you?")
