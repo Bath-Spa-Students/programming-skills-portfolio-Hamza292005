d82c74e8866c1ba6ce4b0d0df97ad2bdc92f9c69
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-15 22:35:02 +0400

    Create Chap 4 ex 3.py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 ex 3.py
@@ -0,0 +1,31 @@
+#Version 1
+alien_colour = "green"
+
+#making if else-if
+if alien_colour == "green":
+  print("You just earned 5 points")
+elif alien_colour == "yellow":
+  print("You just earned 10 points")  
+elif alien_colour == "red":
+  print("You just earned 15 points")
+   
+#version 2
+alien_colour = "yellow"   
+
+if alien_colour == "green":
+  print("You just earned 5 points")
+elif alien_colour == "yellow":
+  print("You just earned 10 points")  
+elif alien_colour =="red":
+  print("You just earned 15 points")
+  
+#version 3
+
+alien_colour = "red" 
+
+if alien_colour == "green":
+  print("You just earned 5 points")
+elif alien_colour == "yellow":
+  print("You just earned 10 points")  
+elif alien_colour =="red":
+  print("You just earned 15 points")
