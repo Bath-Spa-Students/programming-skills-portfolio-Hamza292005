2cc2f8c8c32250291095e8290b55a07736d9ae85
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-15 22:35:39 +0400

    Create Chap 4 ex 5.py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 ex 5.py
@@ -0,0 +1,15 @@
+#Making a list of favorite fruits
+fav_fruits = ["Kiwis","Oranges","Guava"]
+
+#Checking for specific fruits in a list
+if "banana" in fav_fruits:
+    print("you really like bananas")
+if "Oranges" in fav_fruits:
+    print("you really like Oranges")   
+if "Kiwis" in fav_fruits:
+    print("you really like Kiwis")        
+if "Guava" in fav_fruits:
+    print("you really like Guavas")  
+if "Apples" in fav_fruits:
+    print("you really like Apples")      
+          
