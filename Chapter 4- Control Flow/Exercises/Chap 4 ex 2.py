4a346ff264d3495ef222eaa4f72a0dedbb523baf
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-15 22:34:58 +0400

    Create Chap 4 ex 2.py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 ex 2.py
@@ -0,0 +1,18 @@
+# Making version 1
+alien_colour = "orange"
+
+# Creating an if else statement
+if alien_colour =="green":
+  print("You just earned 5 points for shooting the alien")
+  
+else:
+  print("You just earned 10 points")
+
+#version (2) that runs the if block  
+alien_colour = "green"
+
+if alien_colour =="green":
+  print("You just earned 5 points for shooting the alien")
+  
+else:
+  print("You just earned 10 points")
