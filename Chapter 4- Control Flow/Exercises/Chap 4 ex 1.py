ac04a35b06464eaf339a94b32617630124a5a014
Hamza292005 <hamziahmedkhan@gmail.com>
2024-03-15 22:34:51 +0400

    Create Chap 4 ex 1.py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 ex 1.py
@@ -0,0 +1,15 @@
+#Creating a variable named alien_colour and assigning a value
+
+alien_colour = "red"
+
+#making an if statement
+if alien_colour == "green":
+  print("You just earned 5 points")
+  
+#second version where if statement comes true
+
+  alien_colour = "red"
+
+if alien_colour == "red":
+  print("You just earned 5 points")
+  
